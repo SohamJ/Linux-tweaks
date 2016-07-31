b09a53245dbff0e8a23c5839164e03b9f9e9e949
SohamJ <soham4jam@gmail.com>
2016-07-31 10:25:37 +0530

    add zipalign and aapt

--- installation.sh
+++ installation.sh
@@ -18,7 +18,8 @@ sudo apt-get install openjdk-7-jre
 apt-get install git-core gnupg flex bison gperf build-essential \
   zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 \
   lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev ccache \
-  libgl1-mesa-dev libxml2-utils xsltproc unzip python-lunch schedtool
+  libgl1-mesa-dev libxml2-utils xsltproc unzip python-lunch schedtool \
+  aapt zipalign
 
 
 #ADB
