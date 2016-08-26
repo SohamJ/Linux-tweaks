316ddc3a9ebf39435aa2e03636ed0960f151f08e
Soham Jambhekar <soham4jam@gmail.com>
2016-08-26 11:53:32 +0530

    Add arc theme

--- installation.sh
+++ installation.sh
@@ -171,3 +171,7 @@ sudo add-apt-repository ppa:pdfchain-team/ppa
 sudo apt-get update
 sudo apt-get install pdfchain
 
+#Arc Theme
+sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.04/ /' >> /etc/apt/sources.list.d/arc-theme.list"
+sudo apt-get update
+sudo apt-get install arc-theme
