0f1180a0e38a4c6eb423449c5c52f972c03d5a83
SohamJ <soham4jam@gmail.com>
2016-09-03 14:03:32 +0530

    Add Gprename

--- installation.sh
+++ installation.sh
@@ -178,3 +178,6 @@ sudo apt-get install pdfchain
 sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.04/ /' >> /etc/apt/sources.list.d/arc-theme.list"
 sudo apt-get update
 sudo apt-get install arc-theme
+
+#Gprename
+sudo apt-get install gprename
