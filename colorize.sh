05592d8c437105023cfbef394e0d37aae2d6cdcb
SohamJ <soham4jam@gmail.com>
2016-09-03 13:14:53 +0530

    Improve the colorize script Add option to pass the filename and fuzz value Make 40 as the default fuzz value Update desc.

--- colorize.sh
+++ colorize.sh
@@ -1,17 +1,25 @@
 #Author - Soham
 
 #This script batch converts a specific color found in all the pngs to user-specified color.
-#Usage - ./colorize.sh <path> <Color you want in hex> <Current color in hex>
-#eg. Changing a teal png to white one ./colorize <path> FFFFFF 009688
+#Usage - ./colorize.sh <path> <filename> <Color you want in hex> <Current color in hex> <fuzzvalue>
+#eg. Changing a teal png to white one ./colorize ~/soham/theme/res/ abc-*.png FFFFFF 009688 80
 
-#The Fuzz value defines how close you want to close to the current color. Default is 20
+#The Fuzz value defines how close you want to close to the current color. Default is 40
 
 #This script is only for changing the fill color. Background/transparency changes have different commands that
 # you can check in man
 
-folder=$1
-for i in $(find $folder -name '*.png'); do
-	new=$(echo $2)
-	current=$(echo $3)
-	convert $i -fuzz 20% -fill \#"$new" -opaque \#"$current" "$i"		
+foldername=$1
+filename=$(echo $2)
+
+if [ $5 == "" ]; then
+	fuzz=40
+else
+	fuzz=$(echo $5)
+fi
+
+for i in $(find $foldername -name $filename); do
+	new=$(echo $3)
+	current=$(echo $4)
+	convert $i -fuzz "$fuzz"% -fill \#"$new" -opaque \#"$current" "$i"		
 done
